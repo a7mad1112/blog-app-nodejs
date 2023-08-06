733c86081e5bfa3fcdfe0891c6ef893b791ca15f
a7mad1112 <ahmalawneh79@gmail.com>
2023-08-06 16:40:34 +0200

    signup

--- src/modules/auth/auth.router.js
+++ src/modules/auth/auth.router.js
@@ -2,6 +2,6 @@ import express from "express";
 import * as authController from "./controller/auth.controller.js";
 const app = express();
 
-app.get("/", authController.getAuth);
+app.post("/signup", authController.signup);
 
 export default app;
--- src/modules/auth/controller/auth.controller.js
+++ src/modules/auth/controller/auth.controller.js
@@ -1,3 +1,12 @@
-export const getAuth = (req, res) => {
-  return res.json({ msg: "Auth" });
+import UserModel from "./../../../../db/models/user.model.js";
+export const signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await UserModel.crate({ userName: name, email, password });
+    return res.json({ msg: "success", data: user });
+  } catch (error) {
+    if (error.original?.errno == 1062)
+      return res.json({ msg: "email already exist" });
+    return res.json({ msg: "error", error });
+  }
 };
