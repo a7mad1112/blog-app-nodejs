b139d559d46ec8b0dc1518c9ad254215072fe8fd
a7mad1112 <ahmalawneh79@gmail.com>
2023-08-11 14:51:13 +0200

    include blog-model

--- src/modules/user/controller/user.controller.js
+++ src/modules/user/controller/user.controller.js
@@ -1,7 +1,13 @@
+import BlogModel from "../../../../db/models/blog.model.js";
 import UserModel from "./../../../../db/models/user.model.js";
 export const getUsers = async (req, res) => {
   try {
-    const users = await UserModel.findAll();
+    const users = await UserModel.findAll({
+      include: {
+        model: BlogModel,
+        attributes: ["title", "body"],
+      },
+    });
     return res.json({ msg: "success", data: users });
   } catch (error) {
     return res.json({ msg: "error", error: error.stack });
